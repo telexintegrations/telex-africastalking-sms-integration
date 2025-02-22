1f4cae01defbdde3fbd4e5d0565eff3f6b3adfb0
Emmanuel Muogbo <muogboemmanuel9@example.com>
2025-02-22 22:25:06 +0100

    update live url

--- src/integrationSpec.ts
+++ src/integrationSpec.ts
@@ -8,7 +8,7 @@ export const integrationSpecSettings = {
       app_description: "An application that enables you to recieve TELEX notifications through your phone SMS.",
       app_logo: "https://res.cloudinary.com/dhspwfrlk/image/upload/v1740242267/l8qhhnzdcluzkoq3xysa.webp",
       app_name: "SMS notifier",
-      app_url: "http://51.20.128.26",
+      app_url: "https://telex-africastalking-integration.onrender.com",
       background_color: "#FFFFFF",
     },
     integration_category: "Communication & Collaboration",
@@ -55,6 +55,6 @@ export const integrationSpecSettings = {
       
 
     ],
-    target_url: "http://51.20.128.26/sms-webhook",
+    target_url: "https://telex-africastalking-integration.onrender.com/sms-webhook",
   },
 };
